507e8dd2fa728f87b170da535f43746048f52f7d
Eman Salem <93897884+EmanMS@users.noreply.github.com>
2024-11-22 15:22:05 +0200

    Add files via upload

---
+++ Task01/Bonus/task01C#/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+
+namespace task01{
+
+    class program {
+        public static void Main(string[] args){
+
+            Console.WriteLine("Hello World! ");
+        }
+
+    }
+}
